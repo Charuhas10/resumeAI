9035edf92fe9ed9157fd6a54a82d9bfb92031280
Charuhas Reddy <charuhasreddybalam@gmail.com>
2023-10-01 18:03:22 +0530

    parse resume route

--- Backend/app.py
+++ Backend/app.py
@@ -1,14 +1,44 @@
-from flask import Flask
+from flask import Flask, jsonify, abort, request
 from flask_cors import CORS
+
+import re
+from pdfminer.high_level import extract_text
+
+import os
+from tempfile import NamedTemporaryFile
 
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return "Hello, World!"
+    return jsonify(message="Hello, World!")
 
 
-if __name__ == '__main__':
+@app.route("/pdf", methods=["POST"])
+def pdf():
+    if "pdf" not in request.files:
+        return jsonify(error="No file part"), 400
+    file = request.files["pdf"]
+    if file.filename == "":
+        return jsonify(error="No selected file"), 400
+    if file:
+        # Save the file to a temporary location
+        temp_file = NamedTemporaryFile(delete=False)
+        file.save(temp_file.name)
+        temp_file.close()  # Close the file
+
+        # Extract text from the saved file
+        text = extract_text(temp_file.name)
+
+        # Clean up (delete) the temporary file
+        os.unlink(temp_file.name)
+
+        # text = re.sub(r"\n", "", text)
+        text = re.sub(r"\s+", " ", text)
+        return jsonify(text=text)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
--- Frontend/src/ResumeUpload.jsx
+++ Frontend/src/ResumeUpload.jsx
@@ -19,6 +19,7 @@ function ResumeUpload() {
   const handleSubmit = (file) => {
     if (file) {
       console.log("File selected: ", file.name);
+      parseData(file);
       // Later here we will make an API call to the backend
     } else {
       console.log("No file selected");
@@ -29,9 +30,47 @@ function ResumeUpload() {
     document.getElementById("fileInput").click();
   };
 
+  // Inside a React component or hook
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/");
+      const data = await response.json();
+      console.log(data.message); // Should log: "Hello from Flask!"
+    } catch (error) {
+      console.error("There was an error fetching the data:", error);
+    }
+  };
+
+  const parseData = async (file) => {
+    const formData = new FormData();
+    formData.append("pdf", file);
+
+    try {
+      const fileData = await fetch("http://localhost:5000/pdf", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!fileData.ok) {
+        throw new Error(`Server responded with ${fileData.status}`);
+      }
+
+      const data = await fileData.json();
+      console.log(data.text);
+    } catch (error) {
+      console.error("There was an error fetching the data:", error);
+    }
+  };
+
+  const handleDivClick = () => {
+    openFileDialog();
+    fetchData();
+    // parseData();
+  };
+
   return (
     <div
-      onClick={openFileDialog}
+      onClick={handleDivClick}
       onDragOver={handleDragOver}
       onDrop={handleDrop}
       style={{
@@ -47,6 +86,7 @@ function ResumeUpload() {
         id="fileInput"
         onChange={handleFileChange}
         style={{ display: "none" }}
+        accept=".pdf"
       />
       <p>Click to Upload or Drag and Drop</p>
     </div>
