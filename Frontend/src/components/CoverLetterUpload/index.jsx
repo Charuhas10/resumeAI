56b0a8862788516f7d09732357409731403b71f2
Charuhas Reddy <charuhasreddybalam@gmail.com>
2023-11-27 00:04:20 +0530

    fixes

--- Frontend/src/components/CoverLetterUpload/index.jsx
+++ Frontend/src/components/CoverLetterUpload/index.jsx
@@ -8,7 +8,7 @@ import { Row, Col } from "react-bootstrap";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-function CoverLetterUpload({DataReceived}) {
+function CoverLetterUpload({ DataReceived }) {
   const [loading, setLoading] = useState(false);
   const [file, setFile] = useState(null);
   const [text, setText] = useState("");
@@ -114,27 +114,27 @@ function CoverLetterUpload({DataReceived}) {
                       fill="none"
                       xmlns="http://www.w3.org/2000/svg"
                     >
-                      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                       <g
                         id="SVGRepo_tracerCarrier"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                       ></g>
                       <g id="SVGRepo_iconCarrier">
                         {" "}
                         <path
                           d="M15 21H9C6.17157 21 4.75736 21 3.87868 20.1213C3 19.2426 3 17.8284 3 15M21 15C21 17.8284 21 19.2426 20.1213 20.1213C19.8215 20.4211 19.4594 20.6186 19 20.7487"
                           stroke="#000000"
-                          stroke-width="1.5"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                         ></path>{" "}
                         <path
                           d="M12 16V3M12 3L16 7.375M12 3L8 7.375"
                           stroke="#000000"
-                          stroke-width="1.5"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                         ></path>{" "}
                       </g>
                     </svg>
