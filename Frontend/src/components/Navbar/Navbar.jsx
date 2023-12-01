f7f6f2e9c3c70859b11293971aab54df03ad127e
Ayaan <ayaan2536@gmail.com>
2023-12-01 11:55:35 +0530

    resumebuilder added

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -38,7 +38,7 @@ function Navbar() {
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="/resume-builder">
                 Resume Builder
               </a>
             </li>
---
+++ Frontend/src/components/ResumeBuilder/index.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+import { Editor } from "./Editor";
+
+const App_style = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  // border: "1px solid red",
+  // height: "100vh",
+};
+
+const headerStyle = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  margin: "20px",
+};
+
+function ResumeBuilder() {
+  const [signal, setSignal] = useState(0);
+
+  return (
+    <div className="App" style={App_style}>
+      <header style={headerStyle}>
+        <h1>Resume Builder</h1>
+        <button onClick={() => setSignal(signal + 1)}>Print</button>
+      </header>
+      <Editor signal={signal} />
+    </div>
+  );
+}
+
+export default ResumeBuilder;
