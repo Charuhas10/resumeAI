2472b4a0041b1fe9a4651cbcf2a6755bf46a5a72
Amaan <amaan14999@gmail.com>
2023-12-12 23:35:38 +0530

    cover letter fix
    Signed-off-by: Amaan <amaan14999@gmail.com>

--- Frontend/src/components/CoverLetterUpload/index.jsx
+++ Frontend/src/components/CoverLetterUpload/index.jsx
@@ -2,6 +2,8 @@ import styles from "./index.module.css";
 import Loader from "../Loader/";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUpload, faFile } from "@fortawesome/free-solid-svg-icons";
 // import Button from "react-bootstrap/Button";
 // Use <Button> in your component
 
@@ -91,7 +93,9 @@ function CoverLetterUpload({ DataReceived }) {
         quickly make a great cover letter and get the job you dream of!
       </p>
       {loading ? (
-        <Loader />
+        <div className={styles.loaderContainer}>
+          <Loader />
+        </div>
       ) : (
         <form onDragOver={handleDragOver} onDrop={handleDrop}>
           <Row>
@@ -109,40 +113,13 @@ function CoverLetterUpload({ DataReceived }) {
                 />
                 <div className={styles.uploadboxel}>
                   <div>
-                    <svg
-                      width="32px"
-                      height="32px"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                      <g
-                        id="SVGRepo_tracerCarrier"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      ></g>
-                      <g id="SVGRepo_iconCarrier">
-                        {" "}
-                        <path
-                          d="M15 21H9C6.17157 21 4.75736 21 3.87868 20.1213C3 19.2426 3 17.8284 3 15M21 15C21 17.8284 21 19.2426 20.1213 20.1213C19.8215 20.4211 19.4594 20.6186 19 20.7487"
-                          stroke="#a8a8a8"
-                          strokeWidth="1.5"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        ></path>{" "}
-                        <path
-                          d="M12 16V3M12 3L16 7.375M12 3L8 7.375"
-                          stroke="#a8a8a8"
-                          strokeWidth="1.5"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        ></path>{" "}
-                      </g>
-                    </svg>
+                    {file ? (
+                      <FontAwesomeIcon icon={faFile} />
+                    ) : (
+                      <FontAwesomeIcon icon={faUpload} />
+                    )}
                   </div>
-
-                  <p>Click to Upload or Drag and Drop</p>
+                  <p>{file ? file.name : "Click to Upload or Drag and Drop"}</p>
                 </div>
               </label>
             </Col>
